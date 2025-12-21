4413ce06840483ab22880b5f3b06fe7178b535f4
Arsh Verma <arshverma.dev@gmail.com>
2025-12-21 20:40:57 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: ['/api/', '/history'],
         },
-        sitemap: 'https://www.simplypdf.com/sitemap.xml',
+        sitemap: 'https://www.simplypdf.vercel.app/sitemap.xml',
     }
 }
